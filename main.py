de606d42b257e314bb9e04639d2be95cee44f37e
Alexey Milogradov <alexey.milogradov@ringteam.com>
2022-03-07 12:38:05 +0200

    Dont use upper casing for local variable

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ r = requests.get("https://www.gutenberg.org/cache/epub/67477/pg67477.txt").text
 
 
 def count_encounters_in_text(text_object, word):
-    LINES = text_object.split("\n\r")
+    lines = text_object.split("\n\r")
     total = 0
-    for index, line in enumerate(LINES):
+    for index, line in enumerate(lines):
         encounters = line.count(word)
         total = total + encounters
         print('{i}th paragraph: {e}'.format(i=index, e=encounters))
