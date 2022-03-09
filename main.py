76522da02f0603d2848eede37daa378febc3463c
Ruslana Mylohradova <ruslana@192-168-0-167.rdsnet.ro>
2022-03-09 13:06:18 +0200

    added command line parameters: link and a search word

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import requests
+import sys
 
-r = requests.get("https://www.gutenberg.org/cache/epub/67477/pg67477.txt").text
-
-
+text_link = sys.argv[1]
+r = requests.get(text_link).text
+word = sys.argv[2]
 def count_encounters_in_text(text_object, word):
-    lines = text_object.split("\n\r")
+    lines = r.split("\n\r")
     total = 0
     for index, line in enumerate(lines):
         encounters = line.count(word)
         total = total + encounters
         print('{i}th paragraph: {e}'.format(i=index, e=encounters))
     print('In the whole text: {t} encounters'.format(t=total))
-
-count_encounters_in_text(r, 'of')
+count_encounters_in_text(r, word)
 
 
 
