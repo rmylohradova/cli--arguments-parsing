f131a718c966401f5fd51e4727eaba0cea044232
Ruslana Mylohradova <ruslana@192-168-0-167.rdsnet.ro>
2022-03-11 12:37:33 +0200

    fixed exec and non-exec files

--- count1.py
+++ count1.py
@@ -1,22 +1,7 @@
 import requests
 import sys
 
-
-def count_encounters_in_text(text_object, word):
-    lines = text_object.split("\n\r")
-    total = 0
-    for index, line in enumerate(lines):
-        encounters = line.count(word)
-        total = total + encounters
-        print('{i}th paragraph: {e}'.format(i=index, e=encounters))
-    print('In the whole text: {t} encounters'.format(t=total))
-
-
-def executing_count():
-    text_link = sys.argv[1]
-    word_to_count = sys.argv[2]
-    text_to_analyse = requests.get(text_link).text
-    count_encounters_in_text(text_to_analyse, word_to_count)
+from count_lib import count_encounters_in_text, executing_count
 
 if __name__ == '__main__':
     executing_count()
---
+++ count_batch.py
@@ -0,0 +1,7 @@
+import requests
+import sys
+from count_lib import count_encounters_in_text
+from count_lib import count_encounters_batch
+
+if __name__ == '__main__':
+    count_encounters_batch()
