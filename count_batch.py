5c8c67ae70cef2db51c1671a6f91fb8b8dd2cd99
Ruslana Mylohradova <ruslana@192-168-0-167.rdsnet.ro>
2022-03-11 12:43:46 +0200

    logically structured now

--- count_batch.py
+++ count_batch.py
@@ -1,7 +1,18 @@
 import requests
 import sys
 from count_lib import count_encounters_in_text
-from count_lib import count_encounters_batch
+
+
+def count_encounters_batch():
+    word_to_count = sys.argv[1]
+    links_file = sys.argv[2]
+    with open(links_file, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            link = line.rstrip("\n")
+            print("Analysing a new text from", link)
+            r = requests.get(link).text
+            count_encounters_in_text(r, word_to_count)
 
 if __name__ == '__main__':
     count_encounters_batch()
--- count_lib.py
+++ count_lib.py
@@ -11,19 +11,3 @@ def count_encounters_in_text(text_object, word):
         print('{i}th paragraph: {e}'.format(i=index, e=encounters))
     print('In the whole text: {t} encounters'.format(t=total))
 
-def executing_count():
-    text_link = sys.argv[1]
-    word_to_count = sys.argv[2]
-    text_to_analyse = requests.get(text_link).text
-    count_encounters_in_text(text_to_analyse, word_to_count)
-
-def count_encounters_batch():
-    word_to_count = sys.argv[1]
-    links_file = sys.argv[2]
-    with open(links_file, 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            link = line.rstrip("\n")
-            print("Analysing a new text from", link)
-            r = requests.get(link).text
-            count_encounters_in_text(r, word_to_count)
