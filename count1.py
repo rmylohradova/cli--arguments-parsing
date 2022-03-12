9a8fd81948482490cc87cab69266bfc5f55c698a
Ruslana Mylohradova <ruslana@192-168-0-167.rdsnet.ro>
2022-03-12 21:19:59 +0200

    added exceptions

--- count1.py
+++ count1.py
@@ -2,11 +2,16 @@ import requests
 import sys
 from count_lib import count_encounters_in_text, counting_total
 import csv
+import re
 
 def executing_count():
     text_link = sys.argv[1]
     word_to_count = sys.argv[2]
     output_format = sys.argv[3]
+    class InvalidArgument(Exception):
+        pass
+    if output_format != '--output-format=print' and output_format != '--output-format=csv':
+        raise InvalidArgument('Error. Try syntax: --output-format=print or --output-format=csv')
     text_to_analyse = requests.get(text_link).text
     encounters_list = count_encounters_in_text(text_to_analyse, word_to_count)
     if 'print' in output_format:
@@ -15,6 +20,11 @@ def executing_count():
         print("The total count for this text", counting_total(encounters_list))
     elif 'csv' in output_format:
         filename_input = sys.argv[4]
+        class InvalidFileName(Exception):
+            pass
+        match = re.search('^--filename=(\w)*(.)[c][s][v]$', filename_input)
+        if not match:
+            raise InvalidFileName("Error. Try syntax: --filename=*.csv")
         filename = filename_input.split('=')[-1]
         with open(filename, 'w') as csvfile:
             fieldnames = ['url', 'word', 'line_index', 'count']
@@ -29,6 +39,5 @@ def executing_count():
 
 if __name__ == '__main__':
     executing_count()
-
 
 
--- count_batch.py
+++ count_batch.py
@@ -2,12 +2,17 @@ import requests
 import sys
 from count_lib import count_encounters_in_text, counting_total
 import csv
+import re
 
 
 def count_encounters_batch():
     word_to_count = sys.argv[1]
     links_file = sys.argv[2]
     output_format = sys.argv[3]
+    class InvalidArgument(Exception):
+        pass
+    if output_format != '--output-format=print' and output_format != '--output-format=csv':
+        raise InvalidArgument('Error. Try syntax: --output-format=print or --output-format=csv')
     with open(links_file, 'r') as f:
         lines = f.readlines()
         for line in lines:
@@ -21,6 +26,11 @@ def count_encounters_batch():
                 print("The total count for this text", counting_total(encounters_list))
             elif 'csv' in output_format:
                 filename_input = sys.argv[4]
+                class InvalidFileName(Exception):
+                    pass
+                match = re.search('^--filename=(\w)*(.)[c][s][v]$', filename_input)
+                if not match:
+                    raise InvalidFileName("Error. Try syntax: --filename=*.csv")
                 filename = filename_input.split('=')[-1]
                 with open(filename, 'a') as csvfile:
                     fieldnames = ['url', 'word', 'line_index', 'count']
