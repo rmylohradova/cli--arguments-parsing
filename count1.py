b89eea1f7bfdd34e8b8158d731d75904058e451f
Ruslana Mylohradova <ruslana@192-168-0-167.rdsnet.ro>
2022-03-12 18:27:03 +0200

    added alternative outputs: save to csv ot print

--- count1.py
+++ count1.py
@@ -1,15 +1,30 @@
 import requests
 import sys
 from count_lib import count_encounters_in_text, counting_total
+import csv
 
 def executing_count():
     text_link = sys.argv[1]
     word_to_count = sys.argv[2]
+    output_format = sys.argv[3]
     text_to_analyse = requests.get(text_link).text
     encounters_list = count_encounters_in_text(text_to_analyse, word_to_count)
-    for i in range(0, len(encounters_list)):
-        print('{i}th paragraph: {e}'.format(i=i, e=encounters_list[i]))
-    print("The total count for this text", counting_total(encounters_list))
+    if 'print' in output_format:
+        for i in range(0, len(encounters_list)):
+            print('{i}th paragraph: {e}'.format(i=i, e=encounters_list[i]))
+        print("The total count for this text", counting_total(encounters_list))
+    elif 'csv' in output_format:
+        filename_input = sys.argv[4]
+        filename = filename_input.split('=')[-1]
+        with open(filename, 'w') as csvfile:
+            fieldnames = ['url', 'word', 'line_index', 'count']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for i in range(0, len(encounters_list)):
+                writer.writerow({'url': text_link, 'word': word_to_count, 'line_index': i, 'count': encounters_list[i]})
+    else:
+        print("The output method is not specified, please choose between 'print' and 'csv'")
+
 
 
 if __name__ == '__main__':
--- count_batch.py
+++ count_batch.py
@@ -1,21 +1,36 @@
 import requests
 import sys
 from count_lib import count_encounters_in_text, counting_total
+import csv
 
 
 def count_encounters_batch():
     word_to_count = sys.argv[1]
     links_file = sys.argv[2]
+    output_format = sys.argv[3]
     with open(links_file, 'r') as f:
         lines = f.readlines()
         for line in lines:
             link = line.rstrip("\n")
-            print("Analysing a new text from", link)
             r = requests.get(link).text
             encounters_list = count_encounters_in_text(r, word_to_count)
-            for i in range(0, len(encounters_list)):
-                print('{i}th paragraph: {e}'.format(i=i, e=encounters_list[i]))
-            print("The total count for this text", counting_total(encounters_list))
+            if 'print' in output_format:
+                print("Analysing a new text from", link)
+                for i in range(0, len(encounters_list)):
+                    print('{i}th paragraph: {e}'.format(i=i, e=encounters_list[i]))
+                print("The total count for this text", counting_total(encounters_list))
+            elif 'csv' in output_format:
+                filename_input = sys.argv[4]
+                filename = filename_input.split('=')[-1]
+                with open(filename, 'a') as csvfile:
+                    fieldnames = ['url', 'word', 'line_index', 'count']
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+                    for i in range(0, len(encounters_list)):
+                        writer.writerow(
+                            {'url': link, 'word': word_to_count, 'line_index': i, 'count': encounters_list[i]})
+            else:
+                print("The output method is not specified, please choose between 'print' and 'csv'")
 
 if __name__ == '__main__':
     count_encounters_batch()
