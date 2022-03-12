ce270a7fcda8cb06c0c4d9b928b68b655483270c
Ruslana Mylohradova <ruslana@192-168-0-167.rdsnet.ro>
2022-03-12 10:50:18 +0200

    removed non needed enumerate

--- count_lib.py
+++ count_lib.py
@@ -4,7 +4,7 @@ import requests
 def count_encounters_in_text(text_object, word):
     lines = text_object.split("\n\r")
     encounters_list = []
-    for index, line in enumerate(lines):
+    for line in lines:
         encounters_in_line = line.count(word)
         encounters_list.append(encounters_in_line)
     return encounters_list
