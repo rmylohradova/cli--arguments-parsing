2e1443fcdf3a025f65e034637fc8db1cc072901a
Ruslana Mylohradova <ruslana@192-168-0-167.rdsnet.ro>
2022-03-13 14:26:26 +0200

    removed code duplication; updated lib

--- count1.py
+++ count1.py
@@ -1,16 +1,5 @@
 import requests
-import sys
-from count_lib import count_encounters_in_text, counting_total
-import csv
-import re
-
-
-class InvalidArgument(Exception):
-    pass
-
-
-class InvalidFileName(Exception):
-    pass
+from count_lib import *
 
 
 def executing_count():
@@ -23,24 +12,8 @@ def executing_count():
     text_to_analyse = requests.get(text_link).text
     encounters_list = count_encounters_in_text(text_to_analyse, word_to_count)
     output_format = output_format_input.split('=')[-1]
-    if output_format == 'print':
-        for i in range(0, len(encounters_list)):
-            print('{i}th paragraph: {e}'.format(i=i, e=encounters_list[i]))
-        print("The total count for this text", counting_total(encounters_list))
-    elif output_format == 'csv':
-        filename_input = sys.argv[4]
-        match = re.search('^--filename=(\w)*(.)[c][s][v]$', filename_input)
-        if not match:
-            raise InvalidFileName("Error. Try syntax: --filename=*.csv")
-        filename = filename_input.split('=')[-1]
-        with open(filename, 'w') as csvfile:
-            fieldnames = ['url', 'word', 'line_index', 'count']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            for i in range(0, len(encounters_list)):
-                writer.writerow({'url': text_link, 'word': word_to_count, 'line_index': i, 'count': encounters_list[i]})
-    else:
-        print("The output method is not specified, please choose between 'print' and 'csv'")
+    list_output_format(encounters_list, output_format, word_to_count, text_link)
+
 
 
 
--- count_lib.py
+++ count_lib.py
@@ -1,5 +1,14 @@
-import requests
+import sys
+import re
+import csv
 
+
+class InvalidArgument(Exception):
+    pass
+
+
+class InvalidFileName(Exception):
+    pass
 
 def count_encounters_in_text(text_object, word):
     lines = text_object.split("\n\r")
@@ -11,3 +20,24 @@ def count_encounters_in_text(text_object, word):
 
 def counting_total(list):
     return sum(list)
+
+def list_output_format(list, format, word, link):
+    if format == 'print':
+        for i in range(0, len(list)):
+            print('{i}th paragraph: {e}'.format(i=i, e=list[i]))
+        print("The total count for this text", counting_total(list))
+    elif format == 'csv':
+        filename_input = sys.argv[4]
+        match = re.search('^--filename=(\w)*(.)[c][s][v]$', filename_input)
+        if not match:
+            raise InvalidFileName("Error. Try syntax: --filename=*.csv")
+        filename = filename_input.split('=')[-1]
+        with open(filename, 'a') as csvfile:
+            fieldnames = ['url', 'word', 'line_index', 'count']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for i in range(0, len(list)):
+                writer.writerow({'url': link, 'word': word, 'line_index': i, 'count': list[i]})
+            return filename
+    else:
+        print("The output method is not specified, please choose between 'print' and 'csv'")
